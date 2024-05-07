aa1c5320cda0785141931a5e1f2c72a6c620e65c
Doelia <kyxalia@gmail.com>
2024-05-07 12:48:35 +0200

    :bug: Fix pointer event header

--- src/app/parts/Header.tsx
+++ src/app/parts/Header.tsx
@@ -9,12 +9,12 @@ export default function Header({fullHeaderMode, shopRef}) {
     }
 
     return (
-        <header className={"fixed w-full z-50 pb-10"}>
-            <div className={"pointer-events-none absolute inset-0 bg-gradient-to-b from-white to-transparent duration-300 " + (fullHeaderMode ? 'opacity-100' : 'opacity-0')}></div>
+        <header className={"fixed w-full z-50"}>
+            <div className={"pointer-events-none h-[200%] absolute inset-0 bg-gradient-to-b from-white to-transparent duration-300 " + (fullHeaderMode ? 'opacity-100' : 'opacity-0')}></div>
             <div className="relative container mx-auto">
-                <div className={"p-5 relative"}>
-                    <div className={"absolute text-center m-auto left-0 right-0 duration-300 opacity-0 " + (fullHeaderMode ? 'md:opacity-100' : 'md:opacity-0')}>
-                        <button className={"lowercase md:text-3xl text-2xl mb-3 text-fg2 " + tartuffo.className}> the french tototte </button>
+                <div className={"px-5 pt-5 relative"}>
+                    <div className={"pointers-event-none absolute text-center m-auto left-0 right-0 duration-300 opacity-0 " + (fullHeaderMode ? 'md:opacity-100' : 'md:opacity-0')}>
+                        <button className={"lowercase text-2xl tracking-tight mb-3 text-fg2 " + tartuffo.className}> the french tototte </button>
                     </div>
                     <div className={"flex justify-between relative"}>
                         <Link href="/" scroll={true}>
--- src/app/parts/Intro.tsx
+++ src/app/parts/Intro.tsx
@@ -23,11 +23,11 @@ export default function Intro({refFirstScreenTrigger, bentoRef}) {
             <div className={"relative w-full"}>
                 <div className={"bg-gradient-to-t from-bg2 from-50% to-transparent absolute w-full h-full z-20"}></div>
                 <div className={"text-center flex flex-col items-center max-w-2xl px-10 relative mx-auto z-30"}>
-                    <h1 className={"lowercase md:text-3xl text-2xl mb-3 text-fg2 " + tartuffo.className} ref={refFirstScreenTrigger}>The french tototte</h1>
+                    <h1 className={"lowercase text-2xl tracking-tight mb-3 text-fg2 " + tartuffo.className} ref={refFirstScreenTrigger}>The french tototte</h1>
                     <img className={"mb-8"} src={"/france.svg"} width={24} height={6} alt={"France"} />
                     <div className={"group relative mb-8"}>
-                        <h2 className={"lowercase text-5xl md:text-7xl text-fg2 " + tartuffo.className}>Pour ceux qui osent etre differents</h2>
-                        <h2 className={"lowercase text-5xl md:text-7xl text-fg2 " + tartuffo.className + " " + textAnimated}>Pour ceux qui osent etre differents</h2>
+                        <h2 className={"lowercase text-5xl md:text-6xl text-fg2 tracking-tight " + tartuffo.className}>Pour ceux qui osent etre differents</h2>
+                        <h2 className={"lowercase text-5xl md:text-6xl text-fg2 tracking-tight " + tartuffo.className + " " + textAnimated}>Pour ceux qui osent etre differents</h2>
                     </div>
                     <SimpleButton title={"Decouvrir"} onClick={btnDecouvrir}></SimpleButton>
                 </div>
